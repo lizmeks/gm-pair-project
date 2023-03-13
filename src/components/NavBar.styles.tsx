497242187ba6beedee322de0fb1f22aa3d419ff0
Wendy <wyou130@gmail.com>
2023-03-13 11:45:03 -0400

    use flex instead of float for navbar

--- src/components/NavBar.styles.tsx
+++ src/components/NavBar.styles.tsx
@@ -3,20 +3,22 @@ import { Link } from "react-router-dom"
 
 export const Navigation = styled.nav`
     padding: 10px;
-    overflow: hidden;
     background-color: lightgrey;
+    display: inline-flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+`
+
+export const LeftRightContainers = styled.div`
+    display: flex;
+    // Adding additional padding on right because "my profile" option unexpectedly flush with right side, will revisit if extra time
+    padding-right: 10px;
 `
 
 export const NavBarOption = styled(Link)`
-    float: left;
     padding: 10px;
     text-decoration: none;
     color: black;
-`
-
-export const NavBarOptionRight = styled(Link)`
-    float: right;
-    padding: 10px;
-    text-decoration: none;
-    color: black;
+    text-align: center;
 `
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,24 +1,28 @@
-import { NavBarOption, NavBarOptionRight, Navigation } from "./NavBar.styles"
+import { NavBarOption, Navigation, LeftRightContainers } from "./NavBar.styles"
 
 
 const NavBar = () => {
     return (
         <Navigation>
-            <NavBarOption to='/'>
-                Home
-            </NavBarOption>
-            <NavBarOption to='/'>
-                    My Medications
-            </NavBarOption>
-            <NavBarOption to='/'>
-                Add a Medication
-            </NavBarOption>
-            <NavBarOption to='/'>
-                About
-            </NavBarOption>
-            <NavBarOptionRight to='/'>
-                My Profile
-            </NavBarOptionRight>
+            <LeftRightContainers>
+                <NavBarOption to='/'>
+                    Home
+                </NavBarOption>
+                <NavBarOption to='/'>
+                        My Medications
+                </NavBarOption>
+                <NavBarOption to='/'>
+                    Add a Medication
+                </NavBarOption>
+                <NavBarOption to='/'>
+                    About
+                </NavBarOption>
+            </LeftRightContainers>
+            <LeftRightContainers>
+                <NavBarOption to='/'>
+                    My Profile
+                </NavBarOption>
+            </LeftRightContainers>
         </Navigation>
     )
 }
