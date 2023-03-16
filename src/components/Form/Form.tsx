646cb7c3aad13a2e06884ef0dd0289461ae8c499
liz <lizmeeker90@gmail.com>
2023-03-16 12:50:39 -0400

    Added Formik component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useFormik, FieldArray } from 'formik';
+import { useFormik, FieldArray, Formik } from 'formik';
 import {
   Root,
   Title,
@@ -39,61 +39,83 @@ const Form = () => {
   return (
     <Root>
       <Title>Add your medication</Title>
-      <AddForm>
-        <InputLabel htmlFor="name">
-          Name
-          <TextFieldInput
-            type="text"
-            id="name"
-            placeholder='Enter medication name'
-          />
-        </InputLabel>
-        <InputLabel htmlFor="description">
-          Description
-          <LargeTextFieldInput
-            id="description"
-            placeholder='Enter medication description. For example:&#10;-Color&#10;-Shape&#10;-Size'
-          />
-        </InputLabel>
-        <InputLabel htmlFor='image'>
-          Upload medication image (optional)
-          <input
-            type='file'
-            accept='image/*'
-            id='image'
-          />
-        </InputLabel>
-        <div>
-          <p>Reminder times</p>
-          {timeInputs.map(element => {
-            return (
-              <input
-                key={element.key}
-                id={`timeInput${element.key}`}
-                type='time'
-                value={element.value}
-                onChange={handleTimeInputChange}
+      <Formik
+        initialValues={{
+          name: '',
+          description: '',
+          image: '',
+        }}
+        onSubmit={(values, actions) => {
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+        { props => (
+          <AddForm onSubmit={props.handleSubmit}>
+            <InputLabel htmlFor="name">
+              Name
+              <TextFieldInput
+                type="text"
+                id="name"
+                placeholder='Enter medication name'
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                value={props.values.name}
               />
-            )
-          })}
-          <button
-            onClick={handleTimeInputIncrease}
-            type='button'
-          >
-            +Add Reminder
-          </button>
-        </div>
-        <InputLabel htmlFor="notification">
-          Notification Preference
-          <SelectInput id="notification">
-            <option value=''>-Please select an option-</option>
-            <option value='email'>Email</option>
-            <option value='text'>Text message</option>
-            <option value='phone'>Phone call</option>
-          </SelectInput>
-        </InputLabel>
-        <SubmitButton type='submit'>Submit</SubmitButton>
-      </AddForm>
+            </InputLabel>
+            <InputLabel htmlFor="description">
+              Description
+              <LargeTextFieldInput
+                id="description"
+                placeholder='Enter medication description. For example:&#10;-Color&#10;-Shape&#10;-Size'
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                value={props.values.description}
+              />
+            </InputLabel>
+            <InputLabel htmlFor='image'>
+              Upload medication image (optional)
+              <input
+                type='file'
+                accept='image/*'
+                id='image'
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                value={props.values.image}
+              />
+            </InputLabel>
+            <div>
+              <p>Reminder times</p>
+              {timeInputs.map(element => {
+                return (
+                  <input
+                    key={element.key}
+                    id={`timeInput${element.key}`}
+                    type='time'
+                    value={element.value}
+                    onChange={handleTimeInputChange}
+                  />
+                )
+              })}
+              <button
+                onClick={handleTimeInputIncrease}
+                type='button'
+              >
+                +Add Reminder
+              </button>
+            </div>
+            <InputLabel htmlFor="notification">
+              Notification Preference
+              <SelectInput id="notification">
+                <option value=''>-Please select an option-</option>
+                <option value='email'>Email</option>
+                <option value='text'>Text message</option>
+                <option value='phone'>Phone call</option>
+              </SelectInput>
+            </InputLabel>
+            <SubmitButton type='submit'>Submit</SubmitButton>
+          </AddForm>
+        )}
+      </Formik>
     </Root>
   )
 }
