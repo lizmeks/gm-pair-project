326c86888894614db6bac7f69795e69cdfd5a8f3
Wendy <wyou130@gmail.com>
2023-03-16 14:18:00 -0400

    add styles for medications page, fetch users and manually set state for one of them to display only their medications

--- src/components/MedicationTile.tsx
+++ src/components/MedicationTile.tsx
@@ -1,22 +1,26 @@
 import { Medication } from "./MyMedications"
+import { MedicationTileContainer, MedicationInfo, MedicationImage } from "./MyMedications.styles"
 
 const MedicationTile = (props: { med: Medication }) => { // can't destructure props directly in argument?
 
     const { med } = props
 
-    const timesToDisplay = med.times.map(t => <li key={t}>{t}</li>)
+    const timesToDisplay = med.times?.map(t => <li key={t}>{t}</li>)
 
     return (
-        <div>
-            <h3>{med.name}</h3>
-            <p>{med.description}</p>
-            <img src={med.image} alt={med.description} />
-            <p>Notifications: {med.preferred_notifications_method}</p>
-            <p>Times:</p>
-            <ul>
-                {timesToDisplay}
-            </ul>
-        </div>
+        <MedicationTileContainer>
+            <MedicationInfo>
+                <h3>{med.name}</h3>
+
+                <p>{med.description}</p>
+                <p>Notifications: {med.preferred_notifications_method}</p>
+                <p>Times:</p>
+                <ul>
+                    {timesToDisplay}
+                </ul>
+            </MedicationInfo>
+            <MedicationImage src={med.image} alt={med.description} />
+        </MedicationTileContainer>
     )
 }
 
--- src/components/MyMedications.tsx
+++ src/components/MyMedications.tsx
@@ -1,7 +1,14 @@
 import { useEffect, useState } from "react"
 import MedicationTile from "./MedicationTile"
+import { MedicationsContainer } from "./MyMedications.styles"
 
-export interface Medication {
+interface User {
+    id: number,
+    name: string,
+    email: string,
+    phone_number: number,
+    password: string
+}export interface Medication {
     id: number,
     name: string,
     description: string,
@@ -13,7 +20,18 @@ export interface Medication {
 
 const MyMedications = () => {
 
+    const [user, setUser] = useState<User | null>(null)
     const [medications, setMedications] = useState<Medication[]>([])
+
+    useEffect(() => {
+        const fetchUsers = () => {
+            let userData = (fetch('http://localhost:3000/users'))
+                .then(res => res.json())
+                .then(data => setUser(data[0]))
+            return userData
+        }
+        fetchUsers()
+    }, [])
 
     useEffect(() => {
         const fetchMedications = () => {
@@ -25,13 +43,17 @@ const MyMedications = () => {
         fetchMedications()
     }, [])
 
-    const medicationsToDisplay = medications.map(med => <MedicationTile med={med} key={med.id} />)
+    const medicationsToDisplay = medications
+        .filter(med => med.user_id === user?.id)
+        .map(med => <MedicationTile med={med} key={med.id} />)
 
     return (
-        <>
-            <p>My Medications page</p>
-            {medicationsToDisplay}
-        </>
+        <MedicationsContainer>
+            <div>
+                <h1>My Medications</h1>
+                {medicationsToDisplay}
+            </div>
+        </MedicationsContainer>
     )
 }
 
